2d42beed0adf7372a1ac63721edf0f50f712afd8
ruru <ruru.dev07@gmail.com>
2025-08-27 14:11:14 +0530

    feat: add shadcn UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
