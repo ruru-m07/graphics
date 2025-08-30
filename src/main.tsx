063f9944a8fc6b9352456eaa6fb9006934cae7d4
ruru <ruru.dev07@gmail.com>
2025-08-30 17:49:38 +0530

    refactor: update favicon and title

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { NuqsAdapter } from 'nuqs/adapters/react';
+import { scan } from 'react-scan';
 import App from './app';
+
+scan({
+  enabled: false,
+});
 
 // biome-ignore lint/style/noNonNullAssertion: fine for this
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <NuqsAdapter>
+      <App />
+    </NuqsAdapter>
   </StrictMode>
 );
